e1abc50b3a3d73cfb8e0ac219e05d83c53367c1c
Vinicius da Silva <viniciusdasilva50@gmail.com>
2018-06-17 11:04:51 -0300

    Created the package.json

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
  * Created by vinicius on 08/10/2017.
  */
 
-let http = require('http');
-let fs = require('fs');
+let http = require('http')
+let fs = require('fs')
 
 http.createServer(function(req, res){
+    console.log('Run with nodemon')
     fs.readFile('./html/index.html', (err,data) => {
-        data = err === null? data : 'Algo de errado não esta certo!!!';
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.write(data);
-        res.end();
-    });
+        data = err === null? data : 'Algo de errado não esta certo!!!'
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+        res.write(data)
+        res.end()
+    })
 }).listen(3000);
